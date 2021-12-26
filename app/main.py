7e57db53d509f1904a9e70442b799e4ab4ae9888
ygz213 <yigitgurbuz0630@outlook.com>
2021-12-26 16:18:19 +0300

    Seperated GUI and handler

--- app/main.py
+++ app/main.py
@@ -2,25 +2,13 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 
+root = tk.Tk()
+root.title('EasyOpenX')
 
-class handler:
+class handler():
     def __init__(self, master):
         self.apps = []
-        
-        
-        self.canvas = tk.Canvas(master, height=700, width=700, bg='white')
-        self.canvas.create_text(100, 50, text='Current Apps:', fill='black', font=('Constantia 15 bold'))   
-        self.canvas.pack()
-        
-        self.frame = tk.Frame(self.canvas, bg='white')
-        self.frame.place(x=0,y=80,relheight=0.8,relwidth=1)
-        
-        self.addAppButton = tk.Button(self.frame, text='Add an app!', command= self.addApp)
-        self.addAppButton.pack(pady=5)
-        
-        self.openAppsButton = tk.Button(self.frame, text='Open apps', command = self.openApps)
-        self.openAppsButton.pack(pady=5)
-           
+
     def addApp(self):
         filename = filedialog.askopenfilename(initialdir='/', title='Select File', filetypes=(('all files', '*.*'),('executables', '*.exe')))
 
@@ -28,16 +16,29 @@ class handler:
         self.addToCanvas(filename)
     
     def addToCanvas(self, fileName):
-        self.label = tk.Label(self.frame, text=fileName, bg='white').pack()
-        
+        self.label = tk.Label(gui_call.frame, text=fileName, bg='white').pack()
+
     def openApps(self):
         for path in self.apps:
             os.startfile(path)
+handler_call = handler(root)
 
+
+class gui():
+    def __init__(self, master):
+        self.canvas = tk.Canvas(master, height=700, width=700, bg='white')
+        self.canvas.create_text(100, 50, text='Current Apps:', fill='black', font=('Constantia 15 bold'))   
+        self.canvas.pack()
+        
+        self.frame = tk.Frame(self.canvas, bg='white')
+        self.frame.place(x=0,y=80,relheight=0.8,relwidth=1)
+        
+        self.addAppButton = tk.Button(self.frame, text='Add an app!', command= handler_call.addApp)
+        self.addAppButton.pack(pady=5)
+        
+        self.openAppsButton = tk.Button(self.frame, text='Open apps', command = handler_call.openApps)
+        self.openAppsButton.pack(pady=5)
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.title('EasyOpenX')
-    main_call = handler(root)
-
+    gui_call = gui(root)
     root.mainloop()

0511913e1cf38a59dc441c4d44d4ceb87b98a8d4
ygz213 <yigitgurbuz0630@outlook.com>
2022-01-04 20:56:12 +0300

    Deleted gui class

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from tkinter import filedialog
 root = tk.Tk()
 root.title('EasyOpenX')
 
+
 class handler():
     def __init__(self, master):
         self.apps = []
@@ -18,7 +19,7 @@ class handler():
         self.addToCanvas(filename)
     
     def addToCanvas(self, fileName):
-        self.label = tk.Label(gui_call.frame, text=fileName, bg='white').pack()
+        self.label = tk.Label(frame, text=fileName, bg='white').pack()
 
     def openApps(self):
         for path in self.apps:
@@ -31,21 +32,16 @@ class handler():
 handler_call = handler(root)
 
 
-class gui():
-    def __init__(self, master):
-        self.canvas = tk.Canvas(master, height=700, width=700, bg='white')
-        self.canvas.create_text(100, 50, text='Current Apps:', fill='black', font=('Constantia 15 bold'))   
-        self.canvas.pack()
-        
-        self.frame = tk.Frame(self.canvas, bg='white')
-        self.frame.place(x=0,y=80,relheight=0.8,relwidth=1)
-        
-        self.addAppButton = tk.Button(self.frame, text='Add an app!', command= handler_call.addApp)
-        self.addAppButton.pack(pady=5)
-        
-        self.openAppsButton = tk.Button(self.frame, text='Open apps', command = handler_call.openApps)
-        self.openAppsButton.pack(pady=5)
+canvas = tk.Canvas(root, height=700, width=700, bg='white')
+canvas.create_text(100, 50, text='Current Apps:', fill='black', font=('Constantia 15 bold'))   
+canvas.pack()
+frame = tk.Frame(canvas, bg='white')
+frame.place(x=0,y=80,relheight=0.8,relwidth=1)
 
-if __name__ == '__main__':
-    gui_call = gui(root)
-    root.mainloop()
+addAppButton = tk.Button(frame, text='Add an app!', command= handler_call.addApp)
+addAppButton.pack(pady=5)
+
+openAppsButton = tk.Button(frame, text='Open apps', command = handler_call.openApps)
+openAppsButton.pack(pady=5)
+
+root.mainloop()

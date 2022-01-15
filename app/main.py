dc04204e544551d0773422176b6a95e930b8cec6
ygz213 <yigitgurbuz0630@outlook.com>
2022-01-15 15:08:51 +0300

    Changed fonts and colors

--- app/main.py
+++ app/main.py
@@ -4,15 +4,27 @@ root = tk.Tk()
 root.title('EasyOpenX')
 
 canvas = tk.Canvas(root, height=700, width=700, bg='white')
-canvas.create_text(100, 50, text='Current Collections:', fill='black', font=('Constantia 15 bold'))
+canvas.create_text(100, 50, text='   Current Collections:', fill='black', font=('Tahoma 15 bold'))
 canvas.pack()
 frame = tk.Frame(canvas, bg='white')
 frame.place(x=0,y=80,relheight=0.8,relwidth=1)
 
-add_collection_button = tk.Button(frame, text='Create collection')
+add_collection_button = tk.Button(frame,
+                                  bg = '#598D9C',
+                                  fg = '#FFF',
+                                  activebackground = '#416873',
+                                  activeforeground = '#FFF',
+                                  relief = 'flat',
+                                  text = 'Create collection')
 add_collection_button.pack(pady=5)
 
-delete_collection_button = tk.Button(frame, text='Delete collection')
+delete_collection_button = tk.Button(frame, 
+                                     bg = '#D62B47',
+                                     fg = '#FFF',
+                                     activebackground = '#96323C',
+                                     activeforeground = '#FFF',
+                                     relief = 'flat',
+                                     text='Delete collection')
 delete_collection_button.pack(pady=5)
 
 

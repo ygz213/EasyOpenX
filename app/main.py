6c94340f33f5c7963bc27dc2e39f3e4e7641fe0b
ygz213 <yigitgurbuz0630@outlook.com>
2022-01-15 14:28:59 +0300

    Changed app-based UI
    I backed up EOX v1.1 code and will use those codes in collection editor

--- app/main.py
+++ app/main.py
@@ -33,16 +33,16 @@ handler_call = handler(root)
 
 
 canvas = tk.Canvas(root, height=700, width=700, bg='white')
-canvas.create_text(100, 50, text='Current Apps:', fill='black', font=('Constantia 15 bold'))
+canvas.create_text(100, 50, text='Current Collections:', fill='black', font=('Constantia 15 bold'))
 canvas.pack()
 frame = tk.Frame(canvas, bg='white')
 frame.place(x=0,y=80,relheight=0.8,relwidth=1)
 
-addAppButton = tk.Button(frame, text='Add an app!', command= handler_call.addApp)
-addAppButton.pack(pady=5)
+add_collection_button = tk.Button(frame, text='Create collection')
+add_collection_button.pack(pady=5)
 
-openAppsButton = tk.Button(frame, text='Open apps', command = handler_call.openApps)
-openAppsButton.pack(pady=5)
+delete_collection_button = tk.Button(frame, text='Delete collection')
+delete_collection_button.pack(pady=5)
 
 
 root.mainloop()

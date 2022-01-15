e09ecd25e96895938f6e22f9fbcb30d57b8adef1
ygz213 <yigitgurbuz0630@outlook.com>
2022-01-15 14:43:51 +0300

    Seperated handler to collection_handler.py

---
+++ app/collection_handler.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import filedialog
+from subprocess import call
+from platform import system
+import os
+
+class handler():
+    def __init__(self, master):
+        self.apps = []
+
+    def addApp(self):
+        filename = filedialog.askopenfilename(initialdir=f'{os.getcwd()}', title='Select file...', filetypes=([("All files", "*.*")]))
+
+        self.apps.append(filename)
+        self.addToCanvas(filename)
+
+    def addToCanvas(self, fileName):
+        self.label = tk.Label(frame, text=fileName, bg='white').pack()
+
+    def openApps(self):
+        for path in self.apps:
+            if system() == 'Windows':
+                os.startfile(path)
+            if system() == 'Darwin':
+                call(('open', path))
+            if system() == 'Linux':
+                call(('xdg-open', path))
--- app/main.py
+++ app/main.py
@@ -1,36 +1,7 @@
-import os
 import tkinter as tk
-from platform import system
-from subprocess import call
-from tkinter import filedialog
 
 root = tk.Tk()
 root.title('EasyOpenX')
-
-
-class handler():
-    def __init__(self, master):
-        self.apps = []
-
-    def addApp(self):
-        filename = filedialog.askopenfilename(initialdir=f'{os.getcwd()}', title='Select file...', filetypes=([("All files", "*.*")]))
-
-        self.apps.append(filename)
-        self.addToCanvas(filename)
-
-    def addToCanvas(self, fileName):
-        self.label = tk.Label(frame, text=fileName, bg='white').pack()
-
-    def openApps(self):
-        for path in self.apps:
-            if system() == 'Windows':
-                os.startfile(path)
-            if system() == 'Darwin':
-                call(('open', path))
-            if system() == 'Linux':
-                call(('xdg-open', path))
-handler_call = handler(root)
-
 
 canvas = tk.Canvas(root, height=700, width=700, bg='white')
 canvas.create_text(100, 50, text='Current Collections:', fill='black', font=('Constantia 15 bold'))
